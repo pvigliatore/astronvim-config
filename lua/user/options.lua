7655c3cefd4ed951a10008a714acea7b7f90122a
Paolo <paolo.vigliatore@gmail.com>
2023-08-31 10:23:10 -0400

    initial config

---
+++ lua/user/options.lua
@@ -0,0 +1,7 @@
+return {
+  opt = {
+    list = true, -- show whitespace characters
+    relativenumber = false,
+    listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+  }
+}
---
+++ lua/user/plugins/trim.lua
@@ -0,0 +1,11 @@
+return {
+  "cappyzawa/trim.nvim",
+  opts = {
+    ft_blocklist = {},
+    patterns = {},
+    trim_on_write = true,
+    trim_trailing = true,
+    trim_last_line = true,
+    trim_first_line = true,
+  }
+}
