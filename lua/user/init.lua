7dd26c4c2c9f467b49b5238735e72ea82760c298
Paolo Vigliatore <paolo@catalyx.io>
2023-11-13 17:37:24 -0500

    Map Opt-Left and Opt-Right to jump backwards/forwards between words

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,14 +1,17 @@
+vim.keymap.set('n', '<A-Left>', 'b')
+vim.keymap.set('n', '<A-Right>', 'w')
+
 return {
-  colorscheme = "everforest",
-  plugins = {
-    {
-      "sainnhe/everforest",
-      init = function() vim.g.everforest_background = "soft" end
-    }, {
-    "sainnhe/gruvbox-material",
-    init = function()
-      vim.g.gruvbox_material_background = "hard"
-    end
-  }
-  }
+    colorscheme = "everforest",
+    plugins = {
+        {
+            "sainnhe/everforest",
+            init = function() vim.g.everforest_background = "soft" end
+        }, {
+        "sainnhe/gruvbox-material",
+        init = function()
+            vim.g.gruvbox_material_background = "hard"
+        end
+    }
+    }
 }
