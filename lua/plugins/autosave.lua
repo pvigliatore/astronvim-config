d8be422f4762883ce449d01274b7c1f8bf56be85
Paolo Vigliatore <paolo@catalyx.io>
2025-11-08 01:51:52 -0500

    Add autosave plugin

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,9 @@
+return {
+  "https://git.sr.ht/~nedia/auto-save.nvim",
+  event = { "BufReadPre" },
+  opts = {
+    events = { "CursorHold", "InsertLeave", "BufLeave" },
+    silent = false,
+    exclude_ft = { "neo-tree" },
+  },
+}
