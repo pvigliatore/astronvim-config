085c5d2da5a85300fa6fb541077d79f33de741e6
Paolo Vigliatore <paolo.vigliatore@alpaca.markets>
2026-03-27 18:54:17 -0400

    Autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -6,5 +6,14 @@ return {
     silent = false,
     exclude_ft = { "codecompanion", "neo-tree" },
     timeout = 100,
+    -- When the save is deferred (timeout > 0), the current buffer may have
+    -- changed by the time save_fn fires (e.g. after BufLeave).  The default
+    -- save_fn blindly runs `:w` on whatever buffer is current at that point,
+    -- which explodes with E382 if the new buffer has a non-empty buftype.
+    save_fn = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      if vim.bo[bufnr].buftype ~= "" then return end
+      vim.cmd("w")
+    end,
   },
 }
